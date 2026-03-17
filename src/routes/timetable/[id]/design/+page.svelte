3f3d8a1d1a7eb76607f39a6a9820e4241d4ad558
CodeSrushti-3105 <158837536+CodeSrushti-3105@users.noreply.github.com>
2026-03-17 21:19:15 +0530

    converted deprecated store to state

--- src/routes/timetable/[id]/design/+page.svelte
+++ src/routes/timetable/[id]/design/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 </script>
 
 <div class="p-8">
 	<h1 class="text-2xl font-bold">Design Timetable</h1>
 	<p class="text-gray-600">
-		Timetable ID: {$page.params.id}
+		Timetable ID: {page.params.id}
 	</p>
 </div>
